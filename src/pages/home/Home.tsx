02f1653549e7a7807086afb4526d08a245c4f16a
Yue Chang <r3399r@gmail.com>
2021-06-28 09:51:22 +0800

    modify for teach

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,7 +1,12 @@
 import style from './Home.module.scss';
 
 const Home = () => {
-  return <div className={style.content}>hi</div>;
+  return (
+    <div>
+      <div className={style.block1}>hi</div>
+      <div className={style.block2}>this is component</div>
+    </div>
+  );
 };
 
 export default Home;
