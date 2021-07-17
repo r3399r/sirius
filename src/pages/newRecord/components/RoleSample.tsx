c8266585a59dedaf880a03d226038e775dcabe26
Yue Chang <r3399r@gmail.com>
2021-07-17 17:45:25 +0800

    improve return button

--- src/pages/newRecord/components/RoleSample.tsx
+++ src/pages/newRecord/components/RoleSample.tsx
@@ -16,7 +16,10 @@ const RoleSample = ({ onClick }: Props) => {
   const [players, setPlayers] = useState<Player[]>([]);
 
   const [currentStep, setCurrentStep] = useState<number>(0);
+  const [whoIsClicked, setWhoIsClicked] = useState<number[]>([]);
   const [counter, setCounter] = useState<number>(0);
+
+  const submitButtonIsDisabled = roleStep.length > 0 && roleStep[currentStep].howMany !== counter;
 
   useEffect(() => {
     setRoleStep(getRoleStep(state.record.type!));
@@ -24,18 +27,30 @@ const RoleSample = ({ onClick }: Props) => {
   }, [state]);
 
   const onNumClick = (i: number) => () => {
-    if (counter === roleStep[currentStep].howMany) return; // 計數器跟 howMany 一樣時直接 return，按了不會反應
+    // 計數器跟 howMany 一樣時直接 return，按了不會反應
+    if (counter === roleStep[currentStep].howMany) return;
 
     const tmpPlayers = cloneDeep(players);
     tmpPlayers[i].role = roleStep[currentStep].roleCode;
     setPlayers(tmpPlayers);
     setCounter(counter + 1);
+    setWhoIsClicked([...whoIsClicked, i]);
   };
 
   const onReturn = () => {
     // 返回只要回到上一步就好，不用刪改暫存的值，直接覆蓋也可
+    const n = roleStep
+      .slice(0, currentStep - 1)
+      .map((v: Role) => v.howMany)
+      .reduce((a: number, b: number) => a + b, 0);
+    setWhoIsClicked(whoIsClicked.slice(0, n));
+
     setCurrentStep(currentStep - 1);
     setCounter(0);
+  };
+
+  const numIsDisabled = (i: number) => {
+    return whoIsClicked.includes(i) || counter === roleStep[currentStep].howMany;
   };
 
   const onSubmit = () => {
@@ -48,16 +63,20 @@ const RoleSample = ({ onClick }: Props) => {
     setCounter(0);
   };
 
+  if (roleStep.length === 0) return <div>loading...</div>;
+
   return (
     <div>
       <div>身分發放</div>
-      <div>{roleStep.length > 0 && roleStep[currentStep].roleName}</div>
+      <div>{roleStep[currentStep].roleName}</div>
       <div>
         {players?.map((v: Player, i: number) => {
           return (
             <div key={i}>
-              <button onClick={onNumClick(i)}>點選</button>
-              id:{v.id}, name:{v.name}, role:{v.role}
+              <button onClick={onNumClick(i)} disabled={numIsDisabled(i)}>
+                點選
+              </button>
+              id:{v.id}, name:{v.name}, temp role:{v.role}
             </div>
           );
         })}
@@ -65,11 +84,7 @@ const RoleSample = ({ onClick }: Props) => {
       <button onClick={onReturn} disabled={currentStep === 0}>
         return
       </button>
-      {/* tslint:disable-next-line */}
-      <button
-        onClick={onSubmit}
-        disabled={roleStep.length > 0 && counter !== roleStep[currentStep].howMany}
-      >
+      <button onClick={onSubmit} disabled={submitButtonIsDisabled}>
         submit
       </button>
     </div>
