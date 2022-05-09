e5355a40919cee416e966b0c9187297f549bfab3
potentia <potentia@potentiadeMacBook-Pro.local>
2022-05-09 19:16:19 +0800

    fix cicd process

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -15,8 +15,6 @@ echo ===========================================================================
 
 echo deploy backend AWS...
 cd ../backend
-npm ci
-npm run pre:deploy
 aws cloudformation package --template-file aws/cloudformation/template.yaml --output-template-file packaged.yaml --s3-bucket y-cf-midway
 aws cloudformation deploy --template-file packaged.yaml --stack-name $project-$env-stack --parameter-overrides Alias=$alias TargetEnvr=$env Project=$project SubDomain=$subDomain Domain=$domain --no-fail-on-empty-changeset --s3-bucket y-cf-midway
 echo ====================================================================================
